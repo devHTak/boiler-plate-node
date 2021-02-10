9b8872140a262dc305c0ad77b05590f0198a411b
devHTak <qkrgusxkr1@gmail.com>
2021-02-10 22:37:06 +0900

    second commit

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://user:1q2w3e4r5t@test-shard-00-00.j35co.mongodb.net:27017,test-shard-00-01.j35co.mongodb.net:27017,test-shard-00-02.j35co.mongodb.net:27017/Test?ssl=true&replicaSet=atlas-3y14yz-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const {User} = require('./models/User')
+
+// application/x-www-form-urlencoded 데이터를 받아올 수 있도록 설정
+app.use(bodyParser.urlencoded({extended: true}))
+// application/json 데이터를 받아올 수 있도록 설정
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://user:1q2w3e4r5t@test-shard-00-00.j35co.mongodb.net:27017,test-shard-00-01.j35co.mongodb.net:27017,test-shard-00-02.j35co.mongodb.net:27017/Test?ssl=true&replicaSet=atlas-3y14yz-shard-0&authSource=admin&retryWrites=true&w=majority', {
+const config = require('./config/key')
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log("MongoDB Connected"))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => {res.send('Hello World')})
+app.get('/', (req, res) => {res.send('Hello World Nodemon')})
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+    user.save((err, doc)=> {
+        if(err) 
+            return res.json({
+                success: false,
+                err
+            })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 app.listen(port, () => {console.log(`Exmple app listening on port ${port}!`)})
 
